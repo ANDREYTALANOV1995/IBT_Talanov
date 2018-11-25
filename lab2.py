76e471d7d18967eeacce9a3b06f1b1f63250a17c
Your Name <you@example.com>
2018-11-26 02:07:11 +0500

    Поиск по слову

---
+++ lab2.py
@@ -0,0 +1,13 @@
+import nltk
+from nltk.tokenize import word_tokenize
+import requests
+from bs4 import BeautifulSoup
+#nltk.download()
+
+res = requests.get('http://lib.ru')
+res = res.content
+res = BeautifulSoup(res)
+res = nltk.Text(word_tokenize(res.text))
+res = res.concordance('ФАНТАСТИКА')
+
+print(res)
