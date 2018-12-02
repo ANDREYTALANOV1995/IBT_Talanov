84193f142c9129b81d14fe0bb45396cd96d522a5
Матросов Игорь Владимирович <matrosov@MOON.LAN>
2018-12-02 18:23:16 +0500

    Топ 100 слов

--- clear.py
+++ clear.py
@@ -2,18 +2,33 @@ import nltk
 import string
 from nltk.corpus import stopwords
 
+file = open('text.txt')
+text = file.read()
+
+file1 = open('text1.txt')
+text1 = file1.read()
+
 def tokenize_me(file_text):
 
-	tokens = nltk.word_tokenize(file_text)
+	tokens = file_text.lower()
+
+	tokens = nltk.word_tokenize(tokens)
 
 	tokens = [i for i in tokens if (i not in string.punctuation)]
 
 	stop_words = stopwords.words('russian')
-	stop_words.extend(['что', 'это', 'так', 'вот', 'быть', 'как', 'В', '—', 'К', 'на', 'вовсе', 'где', 'И', 'А'])
+	stop_words.extend(['что', 'что-то', 'это', 'так', 'вот', 'быть', 'как', 'из-за', 'в', '–', '—', 'к', 'на', 'вовсе', 'где', 'и', 'а', 'те', 'нам'])
+
 	tokens = [i for i in tokens if (i not in stop_words)]
 
-	# cleaning words
 	tokens = [i.replace("«", "").replace("»", "") for i in tokens]
-	return tokens
+
+	tokens = nltk.FreqDist(tok for tok in tokens)
+
+	return tokens.most_common(100)
+
+
+print(tokenize_me(text))
+
 
 
